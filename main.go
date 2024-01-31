bbf0b2fc0f9179c98a264d1663949ec29fe01015
Lim137 <sergei.ignatov2001@inbox.ru>
2024-01-31 13:10:53 +0300

    added healtz path

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("PORT is not found")
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portStr,
+	}
+	log.Println("Server is running on port " + portStr)
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
